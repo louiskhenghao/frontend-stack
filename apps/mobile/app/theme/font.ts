b5fb6fab05102cacffba3ed9ee7ace834316fbc8
hscheah <anvexr@gmail.com>
2022-01-10 04:03:16 +0800

    remove unused files and minor setup

---
+++ apps/mobile/app/theme/font.ts
@@ -0,0 +1,21 @@
+/**
+ * Stores the fonts used in app
+ *
+ * modify the name of font family used in app here
+ * add other font types needed by the app here
+ */
+
+export const font = {
+  /**
+   * regular font used in most content text
+   */
+  regular: 'Roboto-Regular',
+  /**
+   * bolded font
+   */
+  bold: 'Roboto-Bold',
+  /**
+   * font used for screen header
+   */
+  header: 'Roboto-Bold',
+};
