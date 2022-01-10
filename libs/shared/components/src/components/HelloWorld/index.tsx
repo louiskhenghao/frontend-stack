6d1a133e031f732a60cf1e244bf53cebce974844
louiskhenghao <louiskhenghao@gmail.com>
2022-01-10 15:26:45 +0800

    standardize share component

---
+++ libs/shared/components/src/components/HelloWorld/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { tw } from '@frontend-stack/ui-theme';
+
+export interface HelloWorldProps {}
+
+export const HelloWorld: React.FC<HelloWorldProps> = (props) => {
+  return (
+    <View style={tw('bg-primary p-4')}>
+      <Text style={tw('text-white')}>Welcome to HelloWorld!</Text>
+    </View>
+  );
+};
+
+export default HelloWorld;
