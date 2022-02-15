4ff6a3b5012ccd1dc572370b6f80c508f40da877
louiskhenghao <louiskhenghao@gmail.com>
2022-02-16 04:17:45 +0800

    commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,1 @@
-// https://gist.github.com/devinus/f5864db47497110fe286a6e1ca4763ba
-module.exports = {
-  extends: ["@commitlint/config-conventional"]
-};
+module.exports = { extends: ['@commitlint/config-nx-scopes'] };
